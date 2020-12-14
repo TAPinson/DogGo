a30d55c1e866b03e37f8ee779b4fc2c981579669
Travis Pinson <travispinson@gmail.com>
2020-12-14 14:10:16 -0600

    Set up /walks/index

---
+++ DogGo/Controllers/WalksController.cs
@@ -0,0 +1,99 @@
+﻿using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Controllers
+{
+    public class WalksController : Controller
+    {
+        private readonly WalkRepository _walkRepo;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public WalksController(IConfiguration config)
+        {
+            _walkRepo = new WalkRepository(config);
+        }
+
+        // GET: WalksController
+        public ActionResult Index()
+        {
+            List<Walk> walks = _walkRepo.GetAllWalks();
+            return View(walks);
+        }
+
+        // GET: WalksController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WalksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WalksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WalksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WalksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
